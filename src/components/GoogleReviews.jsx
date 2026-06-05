8ffa35a1133e4532bf9e57cd13dbb4601f66a7fb
mateo vincitorio <mateovincitorio@gmail.com>
2026-06-05 04:33:53 -0300

    hago responsive

--- src/components/GoogleReviews.jsx
+++ src/components/GoogleReviews.jsx
@@ -19,15 +19,15 @@ const GoogleReviews = () => {
         </div>
       </div>
 
-      <p className="reviews">({reviews} reseñas)</p>
+      <p className="reviews">({reviews} Reviews)</p>
 
       <a
         className="link"
-        href="https://google.com"
+        href="https://www.google.com/maps/place/Tattoo+Pikotatto/@39.7179195,2.90078,586m/data=!3m2!1e3!5s0x1297c5c9eef7ce27:0x363cbd511f7a8e8f!4m18!1m8!3m7!1s0x1297c5aabdd0e84f:0x35261b043dcdd7ff!2sTattoo+Pikotatto!8m2!3d39.7179154!4d2.9033549!15sChRwaWtvIHRhdHRvbyBtYWxsb3JjYVoWIhRwaWtvIHRhdHRvbyBtYWxsb3JjYZIBC3RhdHRvb19zaG9wmgFEQ2k5RFFVbFJRVU52WkVOb2RIbGpSamx2VDIxc2JGTlhlREJYV0VKbVZHNWFOazFITVU1alZUQjNWVmhPYmxwWVl4QULgAQD6AQQIQRAz!16s%2Fg%2F11mrjg0mwj!3m8!1s0x1297c5aabdd0e84f:0x35261b043dcdd7ff!8m2!3d39.7179154!4d2.9033549!9m1!1b1!15sChRwaWtvIHRhdHRvbyBtYWxsb3JjYVoWIhRwaWtvIHRhdHRvbyBtYWxsb3JjYZIBC3RhdHRvb19zaG9wmgFEQ2k5RFFVbFJRVU52WkVOb2RIbGpSamx2VDIxc2JGTlhlREJYV0VKbVZHNWFOazFITVU1alZUQjNWVmhPYmxwWVl4QULgAQD6AQQIQRAz!16s%2Fg%2F11mrjg0mwj?entry=ttu&g_ep=EgoyMDI2MDYwMS4wIKXMDSoASAFQAw%3D%3D"
         target="_blank"
         rel="noopener noreferrer"
       >
-        VER RESEÑAS →
+       <p className="color link pGoogle">VIEW REVIEWS →</p> 
       </a>
     </div>
   );
--- src/components/Ubicacion.jsx
+++ src/components/Ubicacion.jsx
@@ -18,13 +18,13 @@ const Ubicacion = () => {
                 icon={faLocationDot}
                 style={{ color: 'rgb(208, 200, 92)' }}
               />
-              <h2 className="titulo-ubicacion color">Ubicación</h2>
+              <h2 className="titulo-ubicacion color">Location</h2>
             </div>
             <p className="color">Carrer de Joan Alcover, 36, Palma de Mallorca</p>
             <div className="maps">
 
             <a href="https://www.google.com/maps/place/Tattoo+Pikotatto/@39.7182785,2.8885062,2346m/data=!3m2!1e3!5s0x1297c5c9eef7ce27:0x363cbd511f7a8e8f!4m10!1m2!2m1!1spiko+tattoo+mallorca!3m6!1s0x1297c5aabdd0e84f:0x35261b043dcdd7ff!8m2!3d39.7179154!4d2.9033549!15sChRwaWtvIHRhdHRvbyBtYWxsb3JjYVoWIhRwaWtvIHRhdHRvbyBtYWxsb3JjYZIBC3RhdHRvb19zaG9wmgFEQ2k5RFFVbFJRVU52WkVOb2RIbGpSamx2VDIxc2JGTlhlREJYV0VKbVZHNWFOazFITVU1alZUQjNWVmhPYmxwWVl4QULgAQD6AQQIQRAz!16s%2Fg%2F11mrjg0mwj?entry=ttu&g_ep=EgoyMDI2MDUyNy4wIKXMDSoASAFQAw%3D%3D" target="_blank" rel="noopener noreferrer" className="color maps">
-                Ver en Google Maps
+                See on Google Maps
             </a>
             <FontAwesomeIcon icon={faArrowRight} style={{color: 'rgb(208, 200, 92)',}} /> 
             </div>
@@ -33,11 +33,11 @@ const Ubicacion = () => {
             <div className="header-horarios">
                 <FontAwesomeIcon icon={faClock} style={{ color: 'rgb(208, 200, 92)' }} />
                 <h3 className="titulo-horarios color">
-                    Horarios
+                    Working Hours
                 </h3>
             </div>
             <div className="horario-content">
-                <p className="color">Martes a Sábado</p>
+                <p className="color">Tuesday to Saturday</p>
                 <p className="color">11:00 - 20:00</p>
             </div>
         </div>
